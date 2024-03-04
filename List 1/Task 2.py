0f027af4892e993d818d9d6608df1d05d6825669
JakubKot01 <j131170k@gmail.com>
2024-03-04 23:41:57 +0100

    Fast recursive approach - dynamic programming

--- List 1/Task 2.py
+++ List 1/Task 2.py
@@ -3,24 +3,31 @@ def calculate_score(text):
     return sum(pow(len(word), 2) for word in words)
 
 
-def split_text(text: str, polish_words_list: set[str], logs_file) -> tuple[int, str]:
-    print(text)
+def split_text(text: str, polish_words_list: set[str], logs_file, memo: dict) -> tuple[int, str]:
+    if text in memo:
+        return memo[text]
+
     if text in polish_words_list:
-        return calculate_score(text), text
+        score = calculate_score(text)
+        memo[text] = (score, text)
+        return score, text
     elif len(text) == 1 and text in polish_words_list:
+        memo[text] = (1, text)
         return 1, text
     elif len(text) <= 1:
+        memo[text] = (0, text)
         return 0, text
 
     best_score: int = 0
     best_split: str = ""
     for i in range(1, len(text)):
-        left_score, left_text = split_text(text[:i], polish_words_list, logs_file)
-        right_score, right_text = split_text(text[i:], polish_words_list, logs_file)
+        left_score, left_text = split_text(text[:i], polish_words_list, logs_file, memo)
+        right_score, right_text = split_text(text[i:], polish_words_list, logs_file, memo)
         if left_score + right_score > best_score:
             best_score = left_score + right_score
             best_split = left_text + " " + right_text
 
+    memo[text] = (best_score, best_split)
     return best_score, best_split
 
 
@@ -29,9 +36,8 @@ def reconstruct_text(input_file_path: str, output_file_path: str, logs_file_path
     output_file = open(output_file_path, 'w', encoding='utf-8')
     logs_file = open(logs_file_path, 'w', encoding='utf-8')
     for line in input_file:
-        print(line)
         line = line.strip()
-        reconstructed_line = split_text(line, polish_words_list, logs_file)
+        reconstructed_line = split_text(line, polish_words_list, logs_file, {})
         output_file.write(reconstructed_line[1].strip() + '\n')
 
 
